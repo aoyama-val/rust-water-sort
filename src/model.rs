e7c4eecebc4f20eb8cfbf3c5499f3caf5d01986f
Aoyama, Shotaro <aoyama@val.co.jp>
2024-03-07 22:46:47 +0900

    tube初期化

--- src/model.rs
+++ src/model.rs
@@ -2,6 +2,9 @@ use rand::prelude::*;
 use std::{num, time};
 
 pub const FPS: i32 = 30;
+pub const TUBE_COUNT: usize = 10;
+pub const MAX_PORTION: usize = 4;
+pub const COLOR_COUNT: usize = TUBE_COUNT - 2;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Command {
@@ -12,6 +15,7 @@ pub struct Game {
     pub rng: StdRng,
     pub is_over: bool,
     pub requested_sounds: Vec<&'static str>,
+    pub tubes: Vec<Vec<i32>>,
 }
 
 impl Game {
@@ -29,12 +33,34 @@ impl Game {
             rng: rng,
             is_over: false,
             requested_sounds: Vec::new(),
+            tubes: Vec::new(),
         };
 
         game
     }
 
-    pub fn init(&mut self) {}
+    pub fn init(&mut self) {
+        let mut portions: Vec<i32> = vec![0; MAX_PORTION * COLOR_COUNT];
+        let mut index = 0;
+        for i in 0..COLOR_COUNT {
+            for _ in 0..MAX_PORTION {
+                portions[index] = i as i32 + 1;
+                index += 1;
+            }
+        }
+        portions.shuffle(&mut self.rng);
+
+        self.tubes = Vec::new();
+
+        for i in 0..COLOR_COUNT {
+            let tube: Vec<i32> = portions[(i * MAX_PORTION)..((i + 1) * MAX_PORTION)].to_vec();
+            self.tubes.push(tube);
+        }
+        for i in 0..(TUBE_COUNT - COLOR_COUNT) {
+            self.tubes.push(Vec::new());
+        }
+        println!("tubes: {:?}", self.tubes);
+    }
 
     pub fn update(&mut self, command: Command) {
         if self.is_over {
